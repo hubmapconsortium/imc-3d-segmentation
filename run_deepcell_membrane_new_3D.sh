fed98fff45e00775546bc45ab1ca1651d4d1e321
Matt Ruffalo <matt.ruffalo@gmail.com>
2022-04-27 10:01:21 -0400

    Remove old shell script

--- run_deepcell_membrane_new_3D.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate deepcell_new
-python deepcell_wrapper_membrane_new_3D.py $1 XY
-python deepcell_wrapper_membrane_new_3D.py $1 YZ
-python deepcell_wrapper_membrane_new_3D.py $1 XZ
-conda deactivate
