5be3a0fdc10344d85bd1c104d9a299f12fe315b8
Matt Ruffalo <matt.ruffalo@gmail.com>
2022-04-27 11:06:41 -0400

    segmentation_2D_slices.py: import and use code instead of shell script

--- segmentation_2d_3d/segmentation_2D_slices.py
+++ segmentation_2d_3d/segmentation_2D_slices.py
@@ -1,14 +1,20 @@
-import os
-from os.path import join
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from skimage.io import imsave
 
+from .deepcell_wrapper_membrane_new_3D import main as deepcell_main
 
-def segmentation(nucleus, cytoplasm, membrane, save_dir):
-    imsave(join(save_dir, "nucleus.tif"), nucleus)
-    imsave(join(save_dir, "cytoplasm.tif"), cytoplasm)
-    imsave(join(save_dir, "membrane.tif"), membrane)
-    os.system("bash run_deepcell_membrane_new_3D.sh " + save_dir)
-    os.system("rm " + join(save_dir, "nucleus.tif"))
-    os.system("rm " + join(save_dir, "cytoplasm.tif"))
-    os.system("rm " + join(save_dir, "membrane.tif"))
+directions = ["XY", "YZ", "XZ"]
+
+
+def segmentation(nucleus, cytoplasm, membrane, save_dir: Path):
+    with TemporaryDirectory() as tmp_dir:
+        tmp_path = Path(tmp_dir)
+
+        imsave(tmp_path / "nucleus.tif", nucleus)
+        imsave(tmp_path / "cytoplasm.tif", cytoplasm)
+        imsave(tmp_path / "membrane.tif", membrane)
+
+        for direction in directions:
+            deepcell_main(tmp_path, direction, save_dir)
