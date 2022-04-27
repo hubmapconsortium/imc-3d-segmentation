efa4b0b3327e4b6d387d49d2a9fa9c8e2e900b58
Matt Ruffalo <matt.ruffalo@gmail.com>
2022-04-27 11:02:09 -0400

    deepcell_wrapper_membrane_new_3D.py: move code to main() to allow importing

--- segmentation_2d_3d/deepcell_wrapper_membrane_new_3D.py
+++ segmentation_2d_3d/deepcell_wrapper_membrane_new_3D.py
@@ -1,76 +1,73 @@
-import sys
-from os.path import join
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from pathlib import Path
 
 import numpy as np
+import tensorflow as tf
 from deepcell.applications import MultiplexSegmentation
 from skimage.io import imread, imsave
-
-file_dir = sys.argv[1]
-axis = sys.argv[2]
-
-im1 = imread(join(file_dir, "nucleus.tif"))
-im2 = imread(join(file_dir, "membrane.tif"))
-z_slice_num = im1.shape[0]
-
-if axis == "XY":
-    pass
-elif axis == "XZ":
-    im1 = np.rot90(im1, k=1, axes=(2, 0))
-    im2 = np.rot90(im2, k=1, axes=(2, 0))
-elif axis == "YZ":
-    im1 = np.rot90(im1, k=1, axes=(1, 0))
-    im2 = np.rot90(im2, k=1, axes=(1, 0))
-
-
-im = np.stack((im1, im2), axis=-1)
-if axis == "XY":
-    pass
-elif axis == "XZ":
-    im_zeros = np.zeros((im.shape[0], im.shape[1], im.shape[0] - im.shape[2], im.shape[3]))
-    im = np.dstack((im, im_zeros))
-elif axis == "YZ":
-    im_zeros = np.zeros((im.shape[0], im.shape[2] - im.shape[1], im.shape[2], im.shape[3]))
-    im = np.hstack((im, im_zeros))
-
-
-import tensorflow as tf
 from tensorflow.compat.v1 import ConfigProto, InteractiveSession
 
-#
-config = ConfigProto()
-config.gpu_options.allow_growth = True
-session = InteractiveSession(config=config)
-config.gpu_options.per_process_gpu_memory_fraction = 0.9
-tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
-app = MultiplexSegmentation()
 
-predictions = [app.predict(np.expand_dims(im[i], 0), compartment="both") for i in range(len(im))]
-labeled_image = np.vstack(predictions)
-for i in range(len(im)):
-    if i == 0:
-        labeled_image = app.predict(np.expand_dims(im[i], 0), compartment="both")
-    else:
-        labeled_image = np.vstack(
-            (labeled_image, app.predict(np.expand_dims(im[i], 0), compartment="both"))
-        )
-print(z_slice_num)
-if axis == "XY":
-    cell_mask = labeled_image[:, :, :, 0]
-    nuc_mask = labeled_image[:, :, :, 1]
-elif axis == "XZ":
-    cell_mask = labeled_image[:, :, :z_slice_num, 0]
-    nuc_mask = labeled_image[:, :, :z_slice_num, 1]
-elif axis == "YZ":
-    cell_mask = labeled_image[:, :z_slice_num, :, 0]
-    nuc_mask = labeled_image[:, :z_slice_num, :, 1]
+def main(file_dir: Path, axis: str):
+    im1 = imread(file_dir / "nucleus.tif")
+    im2 = imread(file_dir / "membrane.tif")
+    z_slice_num = im1.shape[0]
+
+    if axis == "XY":
+        pass
+    elif axis == "XZ":
+        im1 = np.rot90(im1, k=1, axes=(2, 0))
+        im2 = np.rot90(im2, k=1, axes=(2, 0))
+    elif axis == "YZ":
+        im1 = np.rot90(im1, k=1, axes=(1, 0))
+        im2 = np.rot90(im2, k=1, axes=(1, 0))
+
+    im = np.stack((im1, im2), axis=-1)
+    if axis == "XY":
+        pass
+    elif axis == "XZ":
+        im_zeros = np.zeros((im.shape[0], im.shape[1], im.shape[0] - im.shape[2], im.shape[3]))
+        im = np.dstack((im, im_zeros))
+    elif axis == "YZ":
+        im_zeros = np.zeros((im.shape[0], im.shape[2] - im.shape[1], im.shape[2], im.shape[3]))
+        im = np.hstack((im, im_zeros))
+
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    config.gpu_options.per_process_gpu_memory_fraction = 0.9
+    tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+    app = MultiplexSegmentation()
+
+    predictions = [
+        app.predict(np.expand_dims(im[i], 0), compartment="both") for i in range(len(im))
+    ]
+    labeled_image = np.vstack(predictions)
+
+    print(z_slice_num)
+    if axis == "XY":
+        cell_mask = labeled_image[:, :, :, 0]
+        nuc_mask = labeled_image[:, :, :, 1]
+    elif axis == "XZ":
+        cell_mask = labeled_image[:, :, :z_slice_num, 0]
+        nuc_mask = labeled_image[:, :, :z_slice_num, 1]
+    elif axis == "YZ":
+        cell_mask = labeled_image[:, :z_slice_num, :, 0]
+        nuc_mask = labeled_image[:, :z_slice_num, :, 1]
+
+    imsave(file_dir / f"mask_{axis}.tif", cell_mask)
+    imsave(file_dir / f"nuclear_mask_{axis}.tif", nuc_mask)
+
+    print(cell_mask.shape)
+    np.save(file_dir / f"mask_{axis}.npy", cell_mask)
+    np.save(file_dir / f"nuclear_mask_{axis}.npy", nuc_mask)
 
 
-imsave(join(file_dir, "mask_" + axis + ".tif"), cell_mask)
-imsave(join(file_dir, "nuclear_mask_" + axis + ".tif"), nuc_mask)
+if __name__ == "__main__":
+    p = ArgumentParser()
+    p.add_argument("image_dir", type=Path)
+    p.add_argument("axis")
+    args = p.parse_args()
 
-
-print(cell_mask.shape)
-np.save(join(file_dir, "mask_" + axis + ".npy"), cell_mask)
-
-
-np.save(join(file_dir, "nuclear_mask_" + axis + ".npy"), nuc_mask)
+    main(args.image_dir, args.axis)
