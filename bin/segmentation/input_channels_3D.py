54297686b13f0c7db7eeacc32d48a6e25d0ca0b5
Matt Ruffalo <matt.ruffalo@gmail.com>
2022-05-06 14:46:45 -0400

    Use XML-parsing get_channel_names in segmentation code

--- bin/segmentation/input_channels_3D.py
+++ bin/segmentation/input_channels_3D.py
@@ -1,28 +1,9 @@
 from os import fspath
 
-import tifffile
+from aicsimageio import AICSImage
 from skimage.io import imread
 
-
-def get_channel_names(img_dir):
-
-    with tifffile.TiffFile(img_dir) as tif:
-        tif_tags = {}
-        for tag in tif.pages[0].tags.values():
-            name, value = tag.name, tag.value
-            tif_tags[name] = value
-    description = tif_tags["ImageDescription"]
-    name_list = list()
-    for i in range(50):
-        channel_num = "Channel:0:" + str(i)
-        channel_anchor = description.find(channel_num)
-        channel_str = description[channel_anchor : channel_anchor + 80]
-        name_anchor = channel_str.find("Name")
-        name_str = channel_str[name_anchor + 6 : name_anchor + 20]
-        channel_name = name_str[: name_str.find('"')]
-        if len(channel_name) > 0:
-            name_list.append(channel_name)
-    return name_list
+from utils import get_channel_names
 
 
 def IMC_3D_input_channels(img_dir):
@@ -31,7 +12,7 @@ def IMC_3D_input_channels(img_dir):
     # fspath is (currently) necessary to work around a bug in scikit-image
     #              ↓↓↓↓↓↓
     image = imread(fspath(img_dir))
-    channel_names = get_channel_names(img_dir)
+    channel_names = get_channel_names(AICSImage(img_dir))
     nucleus_channel_idx = channel_names.index("Ir191")  # Iridium
     cytoplasm_channel_idx = channel_names.index("In115")  # SMA
     membrane_channel_idx = channel_names.index("La139")  # E-Cad/P-Cad
