259fc37186f1d38a3bc53dee5e2bf9476ac4379e
Matt Ruffalo <matt.ruffalo@gmail.com>
2024-07-23 15:22:34 -0400

    Create mask

--- bin/convert_to_ometiff/convert_to_ometiff.py
+++ bin/convert_to_ometiff/convert_to_ometiff.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Iterable
 
 import aicsimageio.types
-from aicsimageio.writers.ome_tiff_writer import OmeTiffWriter
+import numpy as np
 import ome_utils
 import tifffile
-import numpy as np
+from aicsimageio.writers.ome_tiff_writer import OmeTiffWriter
 
-masks_input = ['cell', 'nuclear']
-channels_output = ['cell', 'nucleus']
+masks_input = ["cell", "nuclear"]
+channels_output = ["cell", "nucleus"]
 
-def read_normalize_mask(path: Path) -> np.ndarray:
-    mask_data = tifffile.imread(path)
+
+def read_normalize_masks(paths: Iterable[Path]) -> np.ndarray:
+    masks = [tifffile.imread(path) for path in paths]
+    mask_data = np.stack(masks)
 
     unique_values = np.unique(mask_data)
     # Is it likely that an entire image will be composed of cells,
@@ -20,7 +23,7 @@ def read_normalize_mask(path: Path) -> np.ndarray:
     if 0 not in unique_values:
         np.insert(unique_values, 0, 0)
 
-    print(f'Mask {path}: shape {mask_data.shape}, {len(unique_values)} objects')
+    print(f"Consolidated mask shape: {mask_data.shape}, {len(unique_values)} objects")
 
     new_mask = np.zeros_like(mask_data, dtype=np.uint32)
     for i, value in enumerate(unique_values):
@@ -28,37 +31,39 @@ def read_normalize_mask(path: Path) -> np.ndarray:
 
     return new_mask
 
+
 def find_expr_image(input_data_dir: Path) -> Path:
     files = list(ome_utils.find_ome_tiffs(input_data_dir))
     if (count := len(files)) != 1:
-        raise ValueError(f'Need 1 OME-TIFF in input directory, found {count}')
+        raise ValueError(f"Need 1 OME-TIFF in input directory, found {count}")
     return files[0]
+
 
 def main(input_data_dir: Path, results_dir: Path):
     expr_image = find_expr_image(input_data_dir)
     with tifffile.TiffFile(expr_image) as tf:
         physical_pixel_sizes = ome_utils.get_converted_physical_size(tf)
     pps = aicsimageio.types.PhysicalPixelSizes(
-        **{dim: size.magnitude for dim, size in physical_pixel_sizes.items()})
+        **{dim: size.magnitude for dim, size in physical_pixel_sizes.items()}
+    )
 
-    mask_data = [
-        read_normalize_mask(results_dir / f'3D_{mask}_mask.tif')
-        for mask in masks_input
-    ]
+    masks = [results_dir / f"3D_{mask}_mask.tif" for mask in masks_input]
+    mask_data = read_normalize_masks(masks)
 
     multichannel_mask = np.stack(mask_data)
     OmeTiffWriter.save(
         multichannel_mask,
-        dim_order='CZYX',
+        dim_order="CZYX",
         channel_names=channels_output,
         physical_pixel_sizes=pps,
-        uri='3D_mask.ome.tiff',
+        uri="3D_mask.ome.tiff",
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     p = ArgumentParser()
-    p.add_argument('input_data_dir', type=Path, help='')
-    p.add_argument('results_dir', type=Path)
+    p.add_argument("input_data_dir", type=Path, help="")
+    p.add_argument("results_dir", type=Path)
     args = p.parse_args()
 
     main(args.input_data_dir, args.results_dir)
